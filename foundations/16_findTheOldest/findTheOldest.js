0a0cd4d4d38aa62fbefcbc820266edbd11fd08ba
Austin D. Loar <austind5717@gmail.com>
2026-07-23 12:03:15 -0400

    Solve exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, person) => {
+    let now = new Date().getFullYear();
+    let oldest_age = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : now - oldest.yearOfBirth;
+    let person_age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : now - person.yearOfBirth;
 
+    if (person_age > oldest_age) {
+      return person;
+    } else {
+      return oldest;
+    }
+  });
 };
 
 // Do not edit below this line
