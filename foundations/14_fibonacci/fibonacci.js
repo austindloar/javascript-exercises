d83d112225bc80566ceb88718636e70fbec1f74d
Austin D. Loar <austind5717@gmail.com>
2026-07-23 11:43:52 -0400

    Solve exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  x = parseInt(x);
 
+  if (x == 0) {
+    return x;
+  } else if (x < 0) {
+    return "OOPS";
+  }
+
+  let previous = 0;
+  let current = 1;
+
+  for (let i = 0; i < x - 1; i++) {
+    let temp = previous;
+    previous = current;
+    current = previous + temp;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
