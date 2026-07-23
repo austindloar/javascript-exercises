e7e0743221ee16f08cecd39a75ed46df0e120066
Austin D. Loar <austind5717@gmail.com>
2026-07-23 11:36:54 -0400

    Solve exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const valid = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  let sanitized = string.toLowerCase().split("").filter(char => valid.includes(char));
+
+  return sanitized.join("") == sanitized.reverse().join("");
 };
 
 // Do not edit below this line
