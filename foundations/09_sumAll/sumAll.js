07ba7572b77396c92cd6080455d166a11c5123f9
Austin D. Loar <austind5717@gmail.com>
2026-07-23 08:27:46 -0400

    Solve exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  for (let i = Math.min(x, y); i <= Math.max(x, y); i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
